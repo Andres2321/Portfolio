fff566257da45723b0dadf8e9422ee70e85e1417
Andres <andres-a23@live.com>
2020-05-08 16:21:54 -0400

    Fixed project 4 link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -97,7 +97,7 @@ function Projects() {
             </div>
           </div>
           <div className="flip-card"
-            onClick={() => { window.open('http://rand-trivia-game.surge.sh/index.html') }}
+            onClick={() => { window.open("animal-rescue.surge.sh "); }}
           >
             <div className="flip-card-inner flex column">
               <div className="flip-card-front flex column">
